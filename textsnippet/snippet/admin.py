8735e47af1ec623a1ac8a60fdc970812fa68556d
vaishakhuk <82841613+vaishakhuk@users.noreply.github.com>
2023-11-05 02:52:22 +0530

    model creation for project
    create textsnippet and tag models

--- textsnippet/snippet/admin.py
+++ textsnippet/snippet/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import TextSnippet
 # Register your models here.
+
+admin.site.register(TextSnippet)
--- textsnippet/snippet/models.py
+++ textsnippet/snippet/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Tag(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.title
+
+class TextSnippet(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE,blank=True, null=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    def __str__(self):
+        return self.title
